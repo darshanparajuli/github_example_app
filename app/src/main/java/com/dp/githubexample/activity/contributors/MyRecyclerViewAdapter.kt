fc7ffa1a565eb2c7f212fdab34affc1333491e1b
Darshan Parajuli <parajulidarshan@gmail.com>
2019-06-23 01:24:23 -0700

    Update icons

--- app/src/main/java/com/dp/githubexample/activity/contributors/MyRecyclerViewAdapter.kt
+++ app/src/main/java/com/dp/githubexample/activity/contributors/MyRecyclerViewAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -36,11 +37,28 @@ internal class MyRecyclerViewAdapter(private val onItemClickListener: OnItemClic
         private val context = v.context
         private val avatar: ImageView = v.findViewById(R.id.avatar)
         private val username: TextView = v.findViewById(R.id.username)
+        private val bio: TextView = v.findViewById(R.id.bio)
         private val contributions: TextView = v.findViewById(R.id.contributions)
+        private val repoCount: TextView = v.findViewById(R.id.repoCount)
+        private val followers: TextView = v.findViewById(R.id.followers)
 
         fun bind(contributor: ContributorInfo) {
-            username.text = contributor.username
+            if (contributor.name != null) {
+                username.text = context.getString(R.string.username_with_name, contributor.username, contributor.name)
+            } else {
+                username.text = contributor.username
+            }
+
+            if (contributor.bio != null) {
+                bio.text = contributor.bio
+                bio.isVisible = true
+            } else {
+                bio.isVisible = false
+            }
+
             contributions.text = context.getString(R.string.integer_x, contributor.contributions)
+            repoCount.text = context.getString(R.string.integer_x, contributor.publicRepos)
+            followers.text = context.getString(R.string.integer_x, contributor.followers)
 
             if (contributor.avatarUrl != null) {
                 Picasso.get()
